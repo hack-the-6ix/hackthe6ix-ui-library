a22543bdf31a60a878f4ffec30589e645d9fdc93
Henry T <henry@henrytu.me>
2020-06-25 23:00:27 -0400

    Created SCSS file for Checkbox

--- src/Checkbox/index.vue
+++ src/Checkbox/index.vue
@@ -8,9 +8,8 @@
     >
       <input
         :class="$style.checkbox__form"
-        @click="$emit('check', !value)"
         type="checkbox"
-        :value="value"
+        v-model="value"
         tabindex="-1"
         :name="name"
       />
@@ -47,60 +46,4 @@ export default {
 };
 </script>
 
-<style lang="scss" module>
-@import '../styles/mixins';
-@import '../styles/variables';
-@import '../styles/colors';
-
-.checkbox {
-  @include flex;
-  margin-bottom: 16px;
-  font-weight: bold;
-
-  &__label {
-    color: $teal;
-    margin-top: 2px;
-    font-size: 0.95rem;
-    font-family: $font;
-  }
-
-  &__container {
-    @include transition(background-color);
-    @include flex(center);
-    margin-right: 10px;
-    position: relative;
-    flex-shrink: 0;
-    width: 22px;
-    height: 22px;
-    border-radius: 6px;
-    overflow: hidden;
-    background-color: #eaeaea;
-    font-size: 0.8rem;
-    color: white;
-
-    &--checked {
-      background-color: $teal;
-    }
-  }
-
-  &__icon {
-    @include transition(opacity transform);
-    transform: rotate(45deg);
-    opacity: 0;
-
-    &--checked {
-      transform: rotate(0deg);
-      opacity: 1;
-    }
-  }
-
-  &__form {
-    @include position(absolute);
-    opacity: 0;
-    z-index: 1;
-    cursor: pointer;
-    width: 100%;
-    height: 100%;
-  }
-}
-</style>
+<style src="./Checkbox.module.scss" lang="scss" module />
