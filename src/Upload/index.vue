39fb2ff001e04f267f9c362f6e3ffc629791290e
fpunny <frederic.pun@gmail.com>
2020-07-02 04:36:44 -0400

    Added file upload

---
+++ src/Upload/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <LabelContainer v-bind="labelContainerProps">
+    <div :class="[$style.container, $style[color]]">
+      <input
+        v-bind="{ ...$listeners, ...$attrs, ...formableProps }"
+        :class="$style.input"
+        @change="onUpload"
+        :accept="accept"
+        type="file"
+      />
+      <div :class="[$style.content, error && $style[`content--error`]]">
+        <Icon :class="$style.icon" name="file-upload" />
+        <p v-if="fileName" :class="$style.label">{{ fileName }}</p>
+        <p v-else>
+          Drop your file here, or <span :class="$style.highlight">browse</span>
+        </p>
+      </div>
+    </div>
+  </LabelContainer>
+</template>
+
+<script>
+import LabelContainerMixin from '../LabelContainer/LabelContainerMixin';
+import formableMixin from '../utils/mixins/formableMixin';
+import LabelContainer from '../LabelContainer';
+import Icon from 'vue-awesome/components/Icon';
+import 'vue-awesome/icons/file-upload';
+import Stack from '../Stack';
+
+export default {
+  name: 'Upload',
+  mixins: [LabelContainerMixin, formableMixin],
+  props: {
+    accept: String,
+    color: {
+      type: String,
+      default: 'teal',
+    },
+  },
+  computed: {
+    fileName() {
+      return this.formValue?.name;
+    },
+  },
+  components: {
+    LabelContainer,
+    Stack,
+    Icon,
+  },
+  methods: {
+    onUpload({ currentTarget }) {
+      this.formHandler({ target: { value: currentTarget.files[0] } });
+    },
+  },
+};
+</script>
+
+<style src="./Upload.module.scss" lang="scss" module />
