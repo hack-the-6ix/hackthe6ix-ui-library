d73862c9f07cc4b935011e00178e2991704670a7
fpunny <me@fpunny.xyz>
2020-07-12 17:58:31 -0400

    Added text component

---
+++ src/Text/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <component
+    :is="tag"
+    :class="[
+      transform && $style[`text--transform-${transform}`],
+      color && $style[`text--color-${color}`],
+      align && $style[`text--align-${align}`],
+      $style[`text--type-${type}`],
+    ]"
+  >
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'Text',
+  props: {
+    transform: String,
+    color: String,
+    align: String,
+    type: String,
+    as: String,
+  },
+  computed: {
+    tag() {
+      return this.$style[this.type] || this.as || 'span';
+    },
+  },
+};
+</script>
+
+<style src="./Text.module.scss" lang="scss" module />
